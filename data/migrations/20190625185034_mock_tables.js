49dc3ec2d626d979b3b71b1f7a20cc1697db9ce2
mjfalahee <mjfalahee@gmail.com>
2019-06-27 10:31:26 -0700

    Fixed mockplanets in migrations

--- data/migrations/20190625185034_mock_tables.js
+++ data/migrations/20190625185034_mock_tables.js
@@ -66,7 +66,7 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return knex.schema
-    .dropTableIfExists('planets')
+    .dropTableIfExists('mockplanets')
     .dropTableIfExists('mockstars')
-    .dropTableIfExists('mockusers');
+    .dropTableIfExists('users');
 };
